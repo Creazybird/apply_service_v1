341ef041e18ae71e3ce1a3996f2ff1a41f9891ec
ShiinaOrez <shiina_orez@qq.com>
2019-09-27 11:09:52 +0800

    [init]

---
+++ app/models.py
@@ -0,0 +1,94 @@
+import time
+import datetime
+from . import db
+from .exceptions import ActivityError
+import werkzeug.http
+from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app, jsonify, request
+from functools import wraps
+
+
+class Project(db.Model):
+    __tablename__ = 'project'
+    id      = db.Column(db.Integer, primary_key=True)
+    name_cn = db.Column(db.String(120))
+    name_en = db.Column(db.String(120))
+    apply_date = db.Column(db.Date)
+    start_date = db.Column(db.Date)
+    end_date   = db.Column(db.Date)
+    status                = db.Column(db.Integer, default=0)
+    abstract_cn           = db.Column(db.Text)
+    abstract_en           = db.Column(db.Text)
+    project_code_1        = db.Column(db.String(8))
+    project_code_2        = db.Column(db.String(8))
+    result_type           = db.Column(db.Integer)
+    apply_funding_count   = db.Column(db.Integer, default=0)
+    work_unit             = db.Column(db.String(120))
+    unit_linkman          = db.Column(db.String(36))
+    unit_linkman_email    = db.Column(db.String(24))
+    unit_linkman_tel      = db.Column(db.String(16))
+    academy_linkman       = db.Column(db.String(36))
+    academy_linkman_email = db.Column(db.String(24))
+    academy_linkman_tel   = db.Column(db.String(16))
+    result_index_list     = db.Column(db.Text)
+
+    project_category_id = db.Column(db.Integer, nullable=False)
+    funding_plan_id = db.Column(db.Integer, nullable=False)
+
+
+class ProjectCategory(db.Model):
+    __tablename__ = 'project_category'
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(16))
+
+    apply_condition_id = db.Column(db.Integer, nullable=False)
+
+
+class Applicant(db.Model):
+    __tablename__ = 'applicant'
+    id = db.Column(db.Integer, primary_key=True)
+    name       = db.Column(db.String(36))
+    sex        = db.Column(db.Boolean)
+    nation     = db.Column(db.String(16))
+    birth_date = db.Column(db.Date)
+    administrative_duty = db.Column(db.String(32))
+    professional_duty   = db.Column(db.String(32))
+    last_degree         = db.Column(db.String(16))
+    specialization      = db.Column(db.String(64))
+    metor    = db.Column(db.String(16))
+    province = db.Column(db.String(20))
+    tel      = db.Column(db.String(16))
+    location = db.Column(db.String(120))
+    zip_code = db.Column(db.String(6))
+
+    accound_id = db.Column(db.Integer, nullable=False)
+
+
+class FundingPlan(db.Model):
+    __tablename__ = 'funding_plan'
+    id = db.Column(db.Integer, primary_key=True)
+    count = db.Column(db.Integer, default=0)
+    device = db.Column(db.Float, default=0.0)
+    data = db.Column(db.Float, default=0.0)
+    meeting = db.Column(db.Float, default=0.0)
+    travel = db.Column(db.Float, default=0.0)
+    working = db.Column(db.Float, default=0.0)
+    manage = db.Column(db.Float, default=0.0)
+    other = db.Column(db.Float, default=0.0)
+    years = db.Column(db.Text)
+
+    condition_id = db.Column(db.Integer, nullable=False)
+    applicant_id = db.Column(db.Integer, nullable=False)
+
+
+class Result(db.Model):
+    __tablename__ = 'result'
+    id = db.Column(db.Integer, primary_key=True)
+    result_name = db.Column(db.String(64))
+    result_type = db.Column(db.Integer, default=0)
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    undertakes = db.Column(db.String(128))
+
+    project_id = db.Column(db.Integer, nullable=False)
---
+++ model.md
@@ -0,0 +1,81 @@
+# Model
+
+## Project 项目
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| name_cn | string | 项目中文名称 |
+| name_en | string | 项目英文名称(保留) |
+| apply_date | date | 项目申报时间(提交申报时间) |
+| start_date | date | 项目开始时间(日) |
+| end_date | date | 项目结束时间(预估) |
+| status | int | 0为正在申报, 1为正在审核, 2为中期检查前, 3为中期检查通过但是项目没有结束, 4为中期检查未通过但是没有结束, 5为项目通过, 6为项目驳回|
+| abstract_cn | text | 项目中文摘要 |
+| abstract_en | text | 项目英文摘要(保留) |
+| project_code_1 | string | 项目代码1 |
+| project_code_2 | string | 项目代码2 |
+| result_type | int | 预期成果类型, 1为专著, 2为译著, 3为论文(集), 4为研究报告, 5为工具书, 6为电脑软件, 7为其他 |
+| apply_funding_count | int | 申请经费总额, 单位为万元 |
+| work_unit | string | 工作单位 |
+| unit_linkman | string | 单位联系人姓名 |
+| unit_linkman_email | string | 联系人邮箱 |
+| unit_linkman_tel | string | 单位联系人电话 |
+| academy_linkman | string | 学院联系人姓名 |
+| academy_linkman_email | string | 学院联系人邮箱 |
+| academy_linkman_tel | string | 学院联系人电话 |
+| result_index_list | string | {"list": [result_id...]} |
+| project_category_id | int | 项目类型ID |
+| funding_plan_id | int | 经费计划ID |
+
+## ProjectCategory 项目类型
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| category_name | string | 项目类别名称 |
+| apply_condition_id | int | 项目申报条件ID |
+
+## Applicant 申报人
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| name | string | 申报人姓名 |
+| sex | boolean | 性别 |
+| nation | string | 民族 |
+| birth_date | date | 生日 |
+| administrative_duty | string | 行政职务 |
+| professional_duty | string | 专业职务 |
+| last_degree | string | 最后学位 |
+| specialization | string | 研究专长 |
+| metor | string | 担任导师 |
+| province | string | 所在省份 |
+| tel | string | 联系电话 |
+| location | string | 通讯地址 |
+| zip_code | string | 邮政编码 |
+| account_id | int | 账户ID |
+
+## FundingPlan 经费计划
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| count | int | 经费总额 |
+| device | float | 设备及耗材 |
+| data | float | 资料费 |
+| meeting | float | 会议费 |
+| travel | float | 差旅费 |
+| working | float | 劳务费 |
+| manange | float | 管理费 |
+| other | float | 其他支出 |
+| years | string | JSON格式的年度支出 |
+| condition_id | int | 审查条件ID |
+| applicant_id | int | 申报人ID |
+
+
+## Result 成果
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| result_name | string | 成果名称 |
+| result_type | int | 不知道有多少 | 
+| start_date | date | 研究阶段开始时间 |
+| end_date | date | 研究阶段结束时间 |
+| undertakers | string | 承担者们 |
