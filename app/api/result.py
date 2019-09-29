3ae06d7161024ec89ffc4a8aa96923b5f79c5383
CGH233 <1113713599@qq.com>
2019-09-29 08:07:50 +0800

    feat；add result part

--- app/api/result.py
+++ app/api/result.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 import json
+import datetime
 from . import api
 from ...app import db
 from ...app.models import Project, Applicant, Result
@@ -12,11 +13,13 @@ def get_results(aid, pid):
         project = Project.query.filter_by(id=pid).first()
         if project != None:
             project_results_list = Result.query.filter_by(project_id=pid).all()
+            result_list = []
+            if project_results_list == None:
+                return jsonify({"result_list":result_list}), 200
             project_results_index_list = []
             for result in project_results_list:
                 project_results_index_list.append(result.id)
             result_index_list = eval(project.result_index_list)
-            result_list = []
             one_result = {
                 "id":"",
                 "name":"",
@@ -41,3 +44,32 @@ def get_results(aid, pid):
                     return jsonify({"msg":"result is inexistent"}), 404
         else:
             return jsonify({"msg":"project is inexistent"}), 404
+
+
+@api.route('/project/<int:pid>/result/add/', methods = ['GET'])
+@Applicant.check
+def add_results(aid, pid):
+    if request.method == "POST":
+        project = Project.query.filter_by(id=pid).first()
+        if project != None:
+            name = request.get_json().get('name')
+            type = request.get_json().get('type')
+            start = request.get_json().get('start')
+            start = datetime.datetime.strptime(start,"%Y%m%d")
+            end = request.get_json().get('end')
+            end = datetime.datetime.strptime(end, "%Y%m%d")
+            result = Result(result_name=name,
+                            result_type=type,
+                            start_date=start,
+                            end_date=end)
+            db.session.add(result)
+            db.session.commit()
+            result_list = eval(project.result_index_list)
+            result_list.append(result.id)
+            project.result_index_list = str(result_list)
+            db.session.add(project)
+            db.session.commit()
+            return jsonify({"msg":"result add successful"}), 200
+        else:
+            return jsonify({"msg": "project is inexistent"}), 404
+
