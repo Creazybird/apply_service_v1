16bdf437531a4101472ba4823d7a242c34b46dd3
CGH233 <1113713599@qq.com>
2019-10-10 10:26:34 +0800

    1

--- manage.py
+++ manage.py
@@ -2,8 +2,7 @@ import sys
 import importlib
 import os
 import time
-from app import create_app, db
-from app.models import User, Activity
+from .app import create_app, db
 from flask_script import Manager, Shell, Command
 from flask_migrate import Migrate, MigrateCommand
 
